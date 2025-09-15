ba9486be957be67dc851cd315aa321b35fc60307
Yazil Ansari <yazilansari@gmail.com>
2025-09-15 15:41:49 +0400

    Changed Condition for Collection

--- platform/plugins/ecommerce/src/Services/CreatePaymentForOrderService.php
+++ platform/plugins/ecommerce/src/Services/CreatePaymentForOrderService.php
@@ -225,30 +225,46 @@ class CreatePaymentForOrderService
                                                                                                                                 <div style="font-family:Helvetica Neue,Helvetica,Roboto,Arial,sans-serif;text-align:left;">&#x631;&#x2E;&#x633; 0.00 (Free Gift)'.'</div>
                                                                                                                             </td>
                                                                                                                         </tr>';
-                                                                                                                    } else if($value->product_category == 'Collections') {
-                                                                                                                        $body .= '<tr>
+                                                                                                                    }
+                                                                                                                    // else if($value->product_category == 'Collections') {
+                                                                                                                    //     $body .= '<tr>
+                                                                                                                    //         <td style="color:#636363;text-align:left;vertical-align:middle;padding:12px;border:1px solid #E5E5E5;">
+                                                                                                                    //             <div style="font-family:Helvetica Neue,Helvetica,Roboto,Arial,sans-serif;text-align:left;">'.$value->product_name.'</div>
+                                                                                                                    //         </td>
+                                                                                                                    //         <td style="color:#636363;text-align:left;vertical-align:middle;padding:12px;border:1px solid #E5E5E5;">
+                                                                                                                    //             <div style="font-family:Helvetica Neue,Helvetica,Roboto,Arial,sans-serif;text-align:left;">'.$value->qty.'</div>
+                                                                                                                    //         </td>
+                                                                                                                    //         <td style="color:#636363;text-align:left;vertical-align:middle;padding:12px;border:1px solid #E5E5E5;">SALES
+                                                                                                                    //             <div style="font-family:Helvetica Neue,Helvetica,Roboto,Arial,sans-serif;text-align:left;">&#x631;&#x2E;&#x633;'.round($value->gross_amount, 2).'</div>
+                                                                                                                    //         </td>
+                                                                                                                    //     </tr>';
+                                                                                                                    // }
+                                                                                                                    else {
+                                                                                                                        if($value->discount_amount != '0') {
+                                                                                                                            $body .= '<tr>
                                                                                                                             <td style="color:#636363;text-align:left;vertical-align:middle;padding:12px;border:1px solid #E5E5E5;">
                                                                                                                                 <div style="font-family:Helvetica Neue,Helvetica,Roboto,Arial,sans-serif;text-align:left;">'.$value->product_name.'</div>
                                                                                                                             </td>
                                                                                                                             <td style="color:#636363;text-align:left;vertical-align:middle;padding:12px;border:1px solid #E5E5E5;">
                                                                                                                                 <div style="font-family:Helvetica Neue,Helvetica,Roboto,Arial,sans-serif;text-align:left;">'.$value->qty.'</div>
                                                                                                                             </td>
-                                                                                                                            <td style="color:#636363;text-align:left;vertical-align:middle;padding:12px;border:1px solid #E5E5E5;">SALES
+                                                                                                                            <td style="color:#636363;text-align:left;vertical-align:middle;padding:12px;border:1px solid #E5E5E5;">
                                                                                                                                 <div style="font-family:Helvetica Neue,Helvetica,Roboto,Arial,sans-serif;text-align:left;">&#x631;&#x2E;&#x633;'.round($value->gross_amount, 2).'</div>
                                                                                                                             </td>
-                                                                                                                        </tr>';
-                                                                                                                    } else {
-                                                                                                                        $body .= '<tr>
-                                                                                                                        <td style="color:#636363;text-align:left;vertical-align:middle;padding:12px;border:1px solid #E5E5E5;">
-                                                                                                                            <div style="font-family:Helvetica Neue,Helvetica,Roboto,Arial,sans-serif;text-align:left;">'.$value->product_name.'</div>
-                                                                                                                        </td>
-                                                                                                                        <td style="color:#636363;text-align:left;vertical-align:middle;padding:12px;border:1px solid #E5E5E5;">
-                                                                                                                            <div style="font-family:Helvetica Neue,Helvetica,Roboto,Arial,sans-serif;text-align:left;">'.$value->qty.'</div>
-                                                                                                                        </td>
-                                                                                                                        <td style="color:#636363;text-align:left;vertical-align:middle;padding:12px;border:1px solid #E5E5E5;">
-                                                                                                                            <div style="font-family:Helvetica Neue,Helvetica,Roboto,Arial,sans-serif;text-align:left;">&#x631;&#x2E;&#x633;'.round((($value->price * 1.15) * $value->qty), 2).'</div>
-                                                                                                                        </td>
-                                                                                                                        </tr>';
+                                                                                                                            </tr>';
+                                                                                                                        } else {
+                                                                                                                            $body .= '<tr>
+                                                                                                                            <td style="color:#636363;text-align:left;vertical-align:middle;padding:12px;border:1px solid #E5E5E5;">
+                                                                                                                                <div style="font-family:Helvetica Neue,Helvetica,Roboto,Arial,sans-serif;text-align:left;">'.$value->product_name.'</div>
+                                                                                                                            </td>
+                                                                                                                            <td style="color:#636363;text-align:left;vertical-align:middle;padding:12px;border:1px solid #E5E5E5;">
+                                                                                                                                <div style="font-family:Helvetica Neue,Helvetica,Roboto,Arial,sans-serif;text-align:left;">'.$value->qty.'</div>
+                                                                                                                            </td>
+                                                                                                                            <td style="color:#636363;text-align:left;vertical-align:middle;padding:12px;border:1px solid #E5E5E5;">
+                                                                                                                                <div style="font-family:Helvetica Neue,Helvetica,Roboto,Arial,sans-serif;text-align:left;">&#x631;&#x2E;&#x633;'.round((($value->price * 1.15) * $value->qty), 2).'</div>
+                                                                                                                            </td>
+                                                                                                                            </tr>';
+                                                                                                                        }
                                                                                                                     }
                                                                                                                 }
                                                                                                                 
